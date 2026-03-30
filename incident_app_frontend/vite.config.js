e4901b5667b38c9255498df3910978712a031856
DNAccept <davidntowanno@outlook.com>
2026-03-30 23:04:23 +0000

    fix: revert base URLs to / to resolve 404 remoteEntry errors

--- incident_app_frontend/vite.config.js
+++ incident_app_frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 
 export default defineConfig({
-  base: 'https://emergency-dispatch-chi.vercel.app/',
+  base: '/',
   plugins: [
     react(),
     federation({
