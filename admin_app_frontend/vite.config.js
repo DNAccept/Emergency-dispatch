7a38db7ea5b2c1a08e5a57f935da24e35c4f5b16
DNAccept <davidntowanno@outlook.com>
2026-03-30 21:15:01 +0000

    fix: set explicit production base URLs for all sub-apps

--- admin_app_frontend/vite.config.js
+++ admin_app_frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 
 export default defineConfig({
+  base: 'https://emergency-admin-app.vercel.app/',
   plugins: [
     react(),
     federation({
